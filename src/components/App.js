8f75dd3346fa0e9de9451986a0d45794a4d0c718
abdelaziz <abdelaziz.iddouch@gmail.com>
2018-09-16 02:52:28 +0100

    did the state part, and created my fish components, now we can add our fishes to the order state

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,59 @@ import Header from './Header';
 import Fish from './Fish';
 import Order from './Order';
 import Enventory from './Enventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+    // Initial State
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    }) 
+  }
+
+  addFish(fish) {
+    // update our state
+    // first make a copy of our state
+    const fishes = {...this.state.fishes};
+    // add in our fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set State
+    this.setState({ fishes });
+  }
+
+  addToOrder(key) {
+    // Take a copy of our state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update our state
+    this.setState({ order });
+  }
+
   render(){
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Fish" />
-          <Fish/>
+          <ul className="list-of-fishes">
+            { Object.keys(this.state.fishes)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />) }
+          </ul> 
         </div>
         <Order/>
-        <Enventory/>
+        <Enventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
     )
   }
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
   render() {
+    const { details, index } = this.props;
+    const isAvailable = details.status === 'available';
+    const btnText = isAvailable ? 'Add To Order' : 'Sold Out'
+
     return (
-      <p>Fish</p>
+      <li className="menu-fish">
+        <img src={ details.image } alt={ details.name } ></img>
+        <h3 className="fish-name">
+          { details.name }
+          <span className="price">{ formatPrice(details.price) }</span>
+        </h3>
+        <p>{ details.desc }</p>
+        <button onClick={() => this.props.addToOrder(index)} disabled={ !isAvailable }>{ btnText }</button>
+      </li>
     )
   }
 }
